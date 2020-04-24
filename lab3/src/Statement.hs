4608e3281c3e9ebe9f098446e0ee02d9011d6a45
Hugo Wangler <hugwan-6@student.ltu.se>
2020-04-24 09:18:05 +0200

    Lab3 done (#3)
    * Init with skeleton code
    
    * Assignment 1 done
    
    * Assignment 2 done
    
    * Assignment 3 done
    
    * Assignment 4 and 5 done
    
    * Implement repeat statement
    
    * Fix repeat implementation
    
    * Add reuse of accept in require
    
    * Fix missing semicolon in repeat test
    
    * Redo implementation of chars
    
    * Add name and format

---
+++ lab3/src/Statement.hs
@@ -0,0 +1,147 @@
+-- Hugo Wangler - hugwan-6
+module Statement
+  ( T
+  , parse
+  , toString
+  , fromString
+  , exec
+  )
+where
+import           Prelude                 hiding ( return
+                                                , fail
+                                                , read
+                                                , repeat
+                                                )
+import           Parser                  hiding ( T )
+import qualified Dictionary
+import qualified Expr
+
+type T = Statement
+
+data Statement =
+  Assignment String Expr.T
+  | Read String
+  | Write Expr.T
+  | Skip
+  | If Expr.T Statement Statement
+  | While Expr.T Statement
+  | Begin [Statement]
+  | Repeat Statement Expr.T
+  deriving Show
+
+assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
+buildAss (v, e) = Assignment v e
+
+read = accept "read" -# word #- require ";" >-> buildRead
+buildRead = Read
+
+write = accept "write" -# Expr.parse #- require ";" >-> buildWrite
+buildWrite = Write
+
+skip = accept "skip" -# require ";" >-> buildSkip
+buildSkip _ = Skip
+
+if' =
+  (accept "if" -# Expr.parse #- require "then")
+    #   (parse #- require "else")
+    #   parse
+    >-> buildIf
+buildIf ((cond, thenStmt), elseStmt) = If cond thenStmt elseStmt
+
+while = (accept "while" -# Expr.parse #- require "do") # parse >-> buildWhile
+buildWhile (cond, doStmt) = While cond doStmt
+
+begin = accept "begin" -# iter parse #- require "end" >-> buildBegin
+buildBegin = Begin
+
+repeat =
+  (accept "repeat" -# parse #- require "until")
+    #   (Expr.parse #- require ";")
+    >-> buildRepeat
+buildRepeat (stmt, untilExpr) = Repeat stmt untilExpr
+
+exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
+exec (If cond thenStmts elseStmts : stmts) dict input =
+  if Expr.value cond dict > 0
+    then exec (thenStmts : stmts) dict input
+    else exec (elseStmts : stmts) dict input
+
+exec (While cond doStmt : stmts) dict input = if Expr.value cond dict > 0
+  then exec (doStmt : While cond doStmt : stmts) dict input
+  else exec stmts dict input
+
+exec (Begin beginStmts : stmts) dict input =
+  exec (beginStmts ++ stmts) dict input
+
+exec (Skip : stmts) dict input = exec stmts dict input
+
+exec (Assignment ident e : stmts) dict input =
+  exec stmts (Dictionary.insert (ident, Expr.value e dict) dict) input
+
+exec (Read ident : stmts) dict input =
+  exec stmts (Dictionary.insert (ident, head input) dict) (tail input)
+
+exec (Write e : stmts) dict input = Expr.value e dict : exec stmts dict input
+
+-- Always executes the repeat once, then creates a new If node that executes if
+-- the until condition is false, otherwise skip 
+exec (Repeat doStmt untilExpr : stmts) dict input =
+  exec (doStmt : If untilExpr Skip (Repeat doStmt untilExpr) : stmts) dict input
+
+exec [] _ _ = []
+
+-- helper fucntion for toString which appends correct number of tabs
+formatToString :: T -> Int -> String
+formatToString (If cond thenStmts elseStmts) tabs =
+  formatTabs tabs
+    ++ "if "
+    ++ Expr.toString cond
+    ++ " then\n"
+    ++ formatToString thenStmts (tabs + 1)
+    ++ "\n"
+    ++ formatTabs tabs
+    ++ "else\n"
+    ++ formatToString elseStmts (tabs + 1)
+
+formatToString (While cond doStmts) tabs =
+  "while " ++ Expr.toString cond ++ " do\n" ++ formatTabs tabs ++ formatToString
+    doStmts
+    (tabs + 1)
+
+formatToString Skip tabs = formatTabs tabs ++ "skip;"
+
+formatToString (Assignment ident e) tabs =
+  formatTabs tabs ++ ident ++ " := " ++ Expr.toString e ++ ";"
+
+formatToString (Begin beginStmts) tabs =
+  formatTabs tabs
+    ++ "begin\n"
+    ++ toStringStmts beginStmts (tabs + 1)
+    ++ "\n"
+    ++ formatTabs tabs
+    ++ "end"
+ where
+  toStringStmts [stmt] tabs = formatToString stmt tabs
+  toStringStmts (stmt : stmts) tabs =
+    formatToString stmt tabs ++ "\n" ++ toStringStmts stmts tabs
+
+formatToString (Write ident) tabs =
+  formatTabs tabs ++ "write " ++ Expr.toString ident ++ ";"
+
+formatToString (Read ident) tabs = formatTabs tabs ++ "read " ++ ident ++ ";"
+
+formatToString (Repeat doStmt untilExpr) tabs =
+  formatTabs tabs
+    ++ "repeat\n"
+    ++ formatToString doStmt (tabs + 1)
+    ++ "\nuntil "
+    ++ Expr.toString untilExpr
+    ++ ";"
+
+formatTabs :: Int -> String
+formatTabs 0 = ""
+formatTabs n = "    " ++ formatTabs (n - 1)
+
+instance Parse Statement where
+  parse = read ! write ! skip ! assignment ! if' ! while ! begin ! repeat
+  toString stmts = formatToString stmts 0
---
+++ lab3/src/TestProgram.hs
@@ -0,0 +1,77 @@
+-- Hugo Wangler - huwan-6
+{- Test for Program -}
+module TestProgram where
+
+import           Program
+p, p1, repeatProgram :: Program.T
+p = fromString
+  ("\
+\read k;\
+\read n;\
+\m := 1;\
+\while n-m do\
+\  begin\
+\    if m - m/k*k then\
+\      skip;\
+\    else\
+\      write m;\
+\    m := m + 1;\
+\  end"
+  )
+
+p1 = fromString
+  ("\
+\read n;\
+\read b;\
+\m := 1;\
+\s := 0;\
+\p := 1;\
+\while n do\
+\  begin\
+\    q := n/b;\
+\    r := n - q*b;\
+\    write r;\
+\    s := p*r+s;\                    
+\    p := p*10;\
+\    n :=q;\
+\  end\
+\write s;"
+  )
+
+sp = putStr (toString p)
+
+rp = Program.exec p [3, 16]
+
+rp1 = Program.exec p1 [1024, 2]
+
+repeatProgram = fromString
+  ("\
+\read n;\
+\s := 0;\
+\repeat\
+\  begin\
+\    s := s + n;\
+\    n := n - 1;\
+\  end\
+\until (0 - n) + 1;\
+\write s;"
+  )
+
+repeatPrint = putStr (toString repeatProgram)
+repeatExec = Program.exec repeatProgram [100] -- sum 1 to 100
+
+pr :: Program.T
+pr = fromString
+  ("\
+\read k;\
+\write k;\
+\repeat\
+\ begin\
+\ k := k + 1;\
+\ write k;\
+\ end\
+\until k;"
+  )
+
+spr = putStr (toString pr)
+rpr k = Program.exec pr [k]
